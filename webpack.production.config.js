601c03bedb77c890cb84282cab1db61a03a31850
AIRBOOK\Admin <bin.liao@medtap.cn>
2017-02-06 21:18:18 +0800

    commit

---
+++ webpack.production.config.js
@@ -0,0 +1,86 @@
+var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+//定义了一些文件夹的路径
+var ROOT_PATH = path.resolve(__dirname);
+var APP_PATH = path.resolve(ROOT_PATH, 'app');
+var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+var webpack = require('webpack');
+var TEM_PATH = path.resolve(APP_PATH, 'templates');
+
+module.exports = {
+	//项目的文件夹 可以直接用文件夹名称 默认会找index.js 也可以确定是哪个文件名字
+	entry: {
+		swiper: APP_PATH + '/js/swiper.js'
+		
+		
+	},
+	//输出的文件名 合并以后的js会命名为bundle.js
+	output: {
+		path: BUILD_PATH,
+		filename: 'js/[name]-min-[hash:8].js'
+	},
+
+	//代码出错后会采用source-map的形式直接显示你出错代码的位置
+	//devtool:'eval-source-map',
+	//定义对模块的处理逻辑,一个对象
+	module: {
+		//定义一系列的加载器使用
+		loaders: [{
+			test: /\.scss$/,
+			loader: ExtractTextPlugin.extract("style", "css!sass", {
+				publicPath: '../'
+			}),//loaders: ['style', 'css'],
+			include: APP_PATH, //字符串或者数组，指包含的文件夹
+			exclude: '' //字符串或数组，指排除的文件夹
+		}, {
+			test: /\.(png|jpg)$/,
+			loader: 'url-loader?limit=4000&name=img/[name].[ext]'
+
+		}, {
+			test: /\.jsx?$/,
+			loader: 'babel',
+			include: APP_PATH,
+			query: {
+				presets: ['es2015']
+			}
+		}],
+		//定义插件一个数组
+
+	},
+	plugins: [
+		/*new HtmlwebpackPlugin({
+			title: '拆红包',
+			template: path.resolve(APP_PATH, 'router.html'),
+			filename: 'router.html',
+			//chunks这个参数告诉插件要引用entry里面的哪几个入口
+			//chunks: ['cj'],
+			//要把script插入到标签里
+			inject: 'body'
+
+		}),*/
+		new ExtractTextPlugin("css/[name]-[hash:8].css"),
+		new webpack.optimize.UglifyJsPlugin({
+			compress: {
+				warnings: false
+			}
+		}),
+		//new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js'),
+
+		//把全局变量插入到所有的代码中，在config.js中配置
+		/*new webpack.ProvidePlugin({
+			$: 'webpack-zepto'
+		}),*/
+		new HtmlwebpackPlugin({
+			title: 'index',
+			template: APP_PATH+'/index.html',
+			filename: 'index.html',
+			//chunks这个参数告诉插件要引用entry里面的哪几个入口
+			chunks: ['swiper'],
+			//要把script插入到标签里
+			inject: 'body'
+
+		})
+
+	]
+}
